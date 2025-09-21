249c0c12d9e880647ff708c0b60f70b94df858d6
gavadams <gav.adams@gmail.com>
2025-09-21 21:31:56 +0100

    build fix

--- src/components/rich-text-editor.tsx
+++ src/components/rich-text-editor.tsx
@@ -6,7 +6,6 @@ import Image from '@tiptap/extension-image'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 import Color from '@tiptap/extension-color'
-import TextStyle from '@tiptap/extension-text-style'
 import Underline from '@tiptap/extension-underline'
 import Placeholder from '@tiptap/extension-placeholder'
 import { 
@@ -84,7 +83,6 @@ export function RichTextEditor({
         types: ['heading', 'paragraph'],
       }),
       Color,
-      TextStyle,
       Underline,
       Placeholder.configure({
         placeholder,
